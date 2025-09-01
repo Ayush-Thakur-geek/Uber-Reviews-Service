72130e41605d1a854f1340644fe39b097b003510
Ayush Thakur <ayush26819@gmail.com>
2025-09-02 01:41:50 +0530

    added eureka discovery client

--- src/main/java/com/uber/UberReviewService/UberReviewServiceApplication.java
+++ src/main/java/com/uber/UberReviewService/UberReviewServiceApplication.java
@@ -3,11 +3,13 @@ package com.uber.UberReviewService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EntityScan("com.uber.UberEntityService.models")
+@EnableDiscoveryClient
 public class UberReviewServiceApplication {
 
 	public static void main(String[] args) {
